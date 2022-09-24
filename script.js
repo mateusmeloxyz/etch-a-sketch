07cc2cfb1b50ec200a0928a8ec03759308803b23
mateusgomes01 <melo.mateus@proton.me>
2022-09-24 15:14:33 -0300

    Fix board to change size dinamically

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+let size = 28;
+
 const board = document.getElementById("board");
 board.style.textAlign = "center";
 
-for( let i = 0 ; i < 16 ; i++ ){
-    for( let j = 0 ; j < 16 ; j++ ){
+for( let i = 0 ; i < size ; i++ ){
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper");
+    for( let j = 0 ; j < size ; j++ ){
         const pixel = document.createElement("div");
-        pixel.classList.add("pixel")
-        board.appendChild(pixel);
+        pixel.classList.add("pixel");
+        wrapper.appendChild(pixel);
         pixel.addEventListener("mouseover", (e) => {
             e.target.classList.add("colored");
         })
     }
+    board.appendChild(wrapper);
 }
