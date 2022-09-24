ae0ff494c6be80de6e25ea072301d7ebf2c95065
Mateus Gomes de Melo <mgm6@ug3c43.windows.cin.ufpe.br>
2022-09-24 12:50:12 -0300

    Add change color event to each pixel

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ board.style.textAlign = "center";
 for( let i = 0 ; i < 16 ; i++ ){
     for( let j = 0 ; j < 16 ; j++ ){
         const pixel = document.createElement("div");
-        pixel.textContent = `${i+j}`;
         pixel.classList.add("pixel")
         board.appendChild(pixel);
+        pixel.addEventListener("mouseover", (e) => {
+            e.target.classList.add("colored");
+        })
     }
 }
